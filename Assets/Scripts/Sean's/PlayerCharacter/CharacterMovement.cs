0670bc5f4f99c71b8ca9a24cab957fef9681b472
der_nabe <34216014+Nabe0416@users.noreply.github.com>
2019-02-28 16:48:26 +0100

    Simple Joystick & Basic movement of character done

---
+++ Assets/Scripts/Sean's/PlayerCharacter/CharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public JoyStick jst;
+
+    private Rigidbody2D rb2d;
+
+    [Range(0.0f, 100.0f)]
+    public float moveSpeed = 20.0f;
+
+    private void Awake()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+    }
+
+    void Walk()
+    {
+        rb2d.MovePosition((Vector2)this.transform.position +  jst.value * Time.deltaTime * moveSpeed);
+    }
+}
---
+++ Assets/Scripts/Sean's/PlayerCharacter/JoyStick.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class JoyStick : ScrollRect
+{
+    //Radius of the joystick.
+    private float rad = 0;
+    //Output value of the joystick.
+    public Vector2 value;
+    
+    void Start()
+    {
+        rad = (transform as RectTransform).sizeDelta.x * 0.45f;
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        //Position of the stick.
+        base.OnDrag(eventData);
+
+    }
+
+    void Update()
+    {
+        JoystickLimit();
+        UpdateOutputValue();
+    }
+
+    /// <summary>
+    /// Restrict the joystick to move inside the given area.
+    /// </summary>
+    void JoystickLimit()
+    {
+        Vector2 pos = content.anchoredPosition;
+        if (pos.magnitude > rad)
+        {
+            pos = pos.normalized * rad;
+            SetContentAnchoredPosition(pos);
+        }
+    }
+    
+    /// <summary>
+    /// Update the output value of the joystick.
+    /// </summary>
+    void UpdateOutputValue()
+    {
+        value = content.localPosition / rad;
+    }
+}
