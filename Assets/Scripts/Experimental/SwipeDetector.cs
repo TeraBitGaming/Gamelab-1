918dcff9160ab5ebb5cae1ee4cf845587702a6ac
TeraBitGaming <loermansw@gmail.com>
2019-04-30 11:26:31 +0200

    V0.8
    [CURRENT]
    Added experimental touch control base; spits out a console message for now.
    
    [FUTURE]
    Adapt the touch control to move character.

---
+++ Assets/Scripts/Experimental/SwipeDetector.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction 
+{
+    none = 0,
+    left = 1,
+    right = 2,
+    up = 4,
+    down = 8,
+
+    upLeft = 5,
+    upRight = 6,
+    downLeft = 9,
+    downRight = 10,
+}
+
+public class SwipeDetector : MonoBehaviour
+{
+    private static SwipeDetector instance;
+    public static SwipeDetector Instance{get {return instance;}}
+    
+
+    public Direction Direction{set;get;}
+
+    private Vector3 touchPos;
+    private float vertSwipeResist = 25.0f;
+    private float horSwipeResist = 15.0f;
+
+    void Start(){
+        instance = this;
+    }
+
+    void FixedUpdate()
+    {
+        Direction = Direction.none;
+
+        if (Input.GetMouseButtonDown (0)){
+            touchPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp (0)){
+            Vector2 deltaSwipe = touchPos - Input.mousePosition;
+
+            if (Mathf.Abs (deltaSwipe.x) > vertSwipeResist){
+                Direction |= (deltaSwipe.x < 0) ? Direction.right : Direction.left;
+            }
+
+            if (Mathf.Abs (deltaSwipe.y) > horSwipeResist){
+                Direction |= (deltaSwipe.y < 0) ? Direction.up : Direction.down;
+            }
+        }
+    }
+
+    public bool IsSwiping(Direction dir) {
+        return (dir == Direction);
+    }
+}
---
+++ Assets/SwipeTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeTester : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (SwipeDetector.Instance.IsSwiping(Direction.left)){
+            Debug.Log("left");
+        }
+        else if (SwipeDetector.Instance.IsSwiping(Direction.right)){
+            Debug.Log("right");
+        }
+        else if (SwipeDetector.Instance.IsSwiping(Direction.up)){
+            Debug.Log("up");
+        }
+        else if (SwipeDetector.Instance.IsSwiping(Direction.down)){
+            Debug.Log("down");
+        }
+        else if (SwipeDetector.Instance.IsSwiping(Direction.none)){
+            Debug.Log("none");
+        }
+        
+    }
+}
