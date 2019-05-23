deb2b9be40eaee0f24ee718bb43e9e8006a78043
TeraBitGaming <loermansw@gmail.com>
2019-05-23 10:21:43 +0200

    V0.6.1
    Explodey v.0.2

--- Assets/ExplodingScript.cs
+++ Assets/ExplodingScript.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class ExplodingScript : MonoBehaviour
 {
+
+
+    private void Explode(){
+        Collider[] hitColliders = Physics.OverlapSphere(Vector3.zero, 10);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            Debug.Log("DAMAGE ME" + hitColliders[i]);
+            i++;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,6 @@ public class ExplodingScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Explode();
     }
 }
