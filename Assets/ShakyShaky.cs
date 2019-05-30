1dbe287769e14b48d80b27d05ead83c547b3f6f4
TeraBitGaming <loermansw@gmail.com>
2019-05-30 15:42:15 +0200

    Music V1.0
    Added a homemade song

---
+++ Assets/ShakyShaky.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakyShaky : MonoBehaviour
+{
+    //This script is largely a Brackey's script, found here: https://www.youtube.com/watch?v=9A9yj8KnM8c
+    private Vector3 originalPosition;
+
+    void Start(){
+        originalPosition = transform.localPosition;
+    }
+
+    public IEnumerator Shake(float duration, float severity)
+    {
+        float elapsed = 0.0f;
+        float shaky;
+
+        while(elapsed < duration)
+        {
+            shaky = 1 - elapsed / duration;
+            float x = Random.Range(-shaky, shaky) * severity;
+            float y = Random.Range(-shaky, shaky) * severity;
+
+            transform.localPosition = new Vector3(x, y, originalPosition.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPosition;
+    }
+}
